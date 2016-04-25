5bc027669b5a6c30dc33a01a4345dedb7bf4fac9
Victor J. C. Geyer <victor@geyer.io>
2016-04-25 12:01:31 +0200

    Added Model\UserInterface

---
+++ src/Ciscaja/Uhsa/UserBundle/Model/UserInterface.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Uhsa package.
+ *
+ * (c) Victor J. C. Geyer <victor@geyer.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ciscaja\Uhsa\UserBundle\Model;
+
+use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
+
+interface UserInterface extends BaseUserInterface
+{
+    /**
+     * @param string $username
+     *
+     * @return UserInterface
+     */
+    public function setUsername($username);
+
+    /**
+     * @param string $password
+     *
+     * @return UserInterface
+     */
+    public function setPassword($password);
+
+    /**
+     * @param string $mail
+     *
+     * @return UserInterface
+     */
+    public function setEmail($mail);
+
+    /**
+     * @return string
+     */
+    public function getEmail();
+
+    /**
+     * @param RoleInterface $role
+     *
+     * @return UserInterface
+     */
+    public function addRole(RoleInterface $role);
+
+    /**
+     * @param RoleInterface $role
+     *
+     * @return UserInterface
+     */
+    public function removeRole(RoleInterface $role);
+
+    /**
+     * @param RoleInterface $role
+     *
+     * @return bool
+     */
+    public function hasRole(RoleInterface $role);
+
+    /**
+     * @return string
+     */
+    public function __toString();
+}
