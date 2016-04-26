0a9ced14e1a505ae1798ace64204858c60bb8406
Victor J. C. Geyer <victor@geyer.io>
2016-04-26 16:50:01 +0200

    Implemented system & user component permission specific methods.

--- src/Ciscaja/Uhsa/UserBundle/Model/Role.php
+++ src/Ciscaja/Uhsa/UserBundle/Model/Role.php
@@ -11,13 +11,21 @@
 
 namespace Ciscaja\Uhsa\UserBundle\Model;
 
+use Ciscaja\Uhsa\UserBundle\Util\BitOperations;
+
 class Role implements RoleInterface
 {
+    use BitOperations;
 
     /**
      * @var string
      */
     protected $role;
+
+    /**
+     * @var int
+     */
+    protected $flags;
 
     /**
      * Role constructor.
@@ -55,5 +63,103 @@ class Role implements RoleInterface
     public function __toString()
     {
         return (string)$this->role;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFlags()
+    {
+        return $this->flags;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canLogin()
+    {
+        return $this->isFlagSet($this->flags, self::FLAG_SYSTEM_LOGIN);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setLoginAllowed($allowed)
+    {
+        $this->setFlag($this->flags, self::FLAG_SYSTEM_LOGIN, $allowed);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canViewUser()
+    {
+        return $this->isFlagSet($this->flags, self::FLAG_USER_VIEW);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setViewUserAllowed($allowed)
+    {
+        $this->setFlag($this->flags, self::FLAG_USER_VIEW, $allowed);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canCreateUser()
+    {
+        return $this->isFlagSet($this->flags, self::FLAG_USER_CREATE);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCreateUserAllowed($allowed)
+    {
+        $this->setFlag($this->flags, self::FLAG_USER_CREATE, $allowed);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canEditUser()
+    {
+        return $this->isFlagSet($this->flags, self::FLAG_USER_EDIT);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setEditUserAllowed($allowed)
+    {
+        $this->setFlag($this->flags, self::FLAG_USER_EDIT, $allowed);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canDeleteUser()
+    {
+        return $this->isFlagSet($this->flags, self::FLAG_USER_DELETE);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDeleteUserAllowed($allowed)
+    {
+        $this->setFlag($this->flags, self::FLAG_USER_DELETE, $allowed);
+
+        return $this;
     }
 }
