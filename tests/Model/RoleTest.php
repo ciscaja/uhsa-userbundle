fd1d4fdf238b162fc9aed61b06e66ab0bdfc1385
Victor J. C. Geyer <victor@geyer.io>
2016-04-26 16:21:21 +0200

    Updated Model\Role test for system & user component specific permission functions.

--- tests/Model/RoleTest.php
+++ tests/Model/RoleTest.php
@@ -19,14 +19,14 @@ class RoleTest extends \PHPUnit_Framework_TestCase
     {
         return new Role();
     }
-    
+
     public function testName()
     {
         $role = self::getRole();
         $this->assertNull($role->getRole());
 
         $role->setRole('ROLE_USER');
-        $this->assertEquals('ROLE_USER',$role->getRole());
+        $this->assertEquals('ROLE_USER', $role->getRole());
     }
 
     public function testSetNameRole()
@@ -41,7 +41,80 @@ class RoleTest extends \PHPUnit_Framework_TestCase
         $this->assertEmpty((string)$role);
 
         $role->setRole('ROLE_USER');
-        $this->assertEquals('ROLE_USER',(string)$role);
+        $this->assertEquals('ROLE_USER', (string)$role);
     }
 
+    /*
+     * Common
+     */
+
+    public function testLoginPermission()
+    {
+        $role = self::getRole();
+        $this->assertFalse($role->canLogin());
+
+        $role->setLoginAllowed(true);
+        $this->assertTrue($role->canLogin());
+    }
+
+    public function testSetLoginPermissionReturn()
+    {
+        $role = self::getRole();
+
+        $this->assertSame($role->setLoginAllowed(true), $role);
+    }
+
+    /*
+     * Uhsa
+     */
+    // create
+    public function testCreateUserPermission()
+    {
+        $role = self::getRole();
+        $this->assertFalse($role->canCreateUser());
+
+        $role->setCreateUserAllowed(true);
+        $this->assertTrue($role->canCreateUser());
+    }
+
+    public function testSetCreateUserPermissionReturn()
+    {
+        $role = self::getRole();
+
+        $this->assertSame($role->setLoginAllowed(true), $role);
+    }
+
+    // edit
+    public function testEditUserPermission()
+    {
+        $role = self::getRole();
+        $this->assertFalse($role->canEditUser());
+
+        $role->setEditUserAllowed(true);
+        $this->assertTrue($role->canEditUser());
+    }
+
+    public function testSetEditUserPermissionReturn()
+    {
+        $role = self::getRole();
+
+        $this->assertSame($role->setLoginAllowed(true), $role);
+    }
+
+    // delete
+    public function testDeleteUserPermission()
+    {
+        $role = self::getRole();
+        $this->assertFalse($role->canDeleteUser());
+
+        $role->setDeleteUserAllowed(true);
+        $this->assertTrue($role->canDeleteUser());
+    }
+
+    public function testSetDeleteUserPermissionReturn()
+    {
+        $role = self::getRole();
+
+        $this->assertSame($role->setLoginAllowed(true), $role);
+    }
 }
