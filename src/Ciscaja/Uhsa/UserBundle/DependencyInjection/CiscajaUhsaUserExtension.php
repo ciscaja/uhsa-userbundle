4274ddf2d7ffc988c045b67925a9e110f5a3c8e8
Victor J. C. Geyer <victor@geyer.io>
2016-04-28 18:10:27 +0200

    Added CiscajaUhsaUserExtension to load the service config forSecurity\UserChecker.

---
+++ src/Ciscaja/Uhsa/UserBundle/DependencyInjection/CiscajaUhsaUserExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Uhsa package.
+ *
+ * (c) Victor J. C. Geyer <victor@geyer.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ciscaja\Uhsa\UserBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+class CiscajaUhsaUserExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config/services')
+        );
+        $loader->load('security.yml');
+    }
+}
