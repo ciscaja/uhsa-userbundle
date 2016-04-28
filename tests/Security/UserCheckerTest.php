2a9149b264f67a96f0ac3083b5ddf109ff36fe99
Victor J. C. Geyer <victor@geyer.io>
2016-04-28 18:59:28 +0200

    Added test for disabled account and canLogin permission for Security\UserChecker

--- tests/Security/UserCheckerTest.php
+++ tests/Security/UserCheckerTest.php
@@ -13,6 +13,7 @@ namespace Ciscaja\Uhsa\UserBundle\Tests\Security;
 
 use Ciscaja\Uhsa\UserBundle\Security\Exception\AccountDeletedException;
 use Ciscaja\Uhsa\UserBundle\Security\Exception\AccountDisabledException;
+use Ciscaja\Uhsa\UserBundle\Security\Exception\AccountRoleNoLoginPermissionException;
 use Ciscaja\Uhsa\UserBundle\Security\UserChecker;
 
 class UserCheckerTest extends \PHPUnit_Framework_TestCase
@@ -39,6 +40,60 @@ class UserCheckerTest extends \PHPUnit_Framework_TestCase
         $user_checker = new UserChecker;
 
         $user_checker->checkPreAuth($user);
+    }
 
+    public function testAccountDisabled()
+    {
+        $this->expectException(AccountDisabledException::class);
+        $user = $this
+            ->getMockBuilder('Ciscaja\Uhsa\UserBundle\Model\User')
+            ->getMock();
+        $user->expects($this->any())
+            ->method('isDeleted')
+            ->will($this->returnValue(false));
+
+        $user->expects($this->any())
+            ->method('isDisabled')
+            ->will($this->returnValue(true));
+
+        $user_checker = new UserChecker;
+
+        $user_checker->checkPreAuth($user);
+    }
+
+    public function testAccountRoleMissingLoginPermission()
+    {
+        $this->expectException(AccountRoleNoLoginPermissionException::class);
+        $user = $this
+            ->getMockBuilder('Ciscaja\Uhsa\UserBundle\Model\User')
+            ->getMock();
+
+        $role1 = $this
+            ->getMockBuilder('Ciscaja\Uhsa\UserBundle\Model\Role')
+            ->getMock();
+        $role1->expects($this->any())
+            ->method('canLogin')
+            ->will($this->returnValue(false));
+        $role2 = $this
+            ->getMockBuilder('Ciscaja\Uhsa\UserBundle\Model\Role')
+            ->getMock();
+        $role2->expects($this->any())
+            ->method('canLogin')
+            ->will($this->returnValue(false));
+
+        $user->expects($this->atLeastOnce())
+            ->method('isDeleted')
+            ->will($this->returnValue(false));
+
+        $user->expects($this->any())
+            ->method('getRoles')
+            ->will($this->returnValue(array(
+                $role1,
+                $role2
+            )));
+
+        $user_checker = new UserChecker;
+
+        $user_checker->checkPreAuth($user);
     }
 }
